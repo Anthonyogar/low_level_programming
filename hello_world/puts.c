1523712a9ffbc94810adf8846365a1ec964c4b78
Anthony Odey <anthonyogar61@gmail.com>
2023-02-15 00:08:38 +0100

    puts

---
+++ hello_world/puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - wrote a program that prints a line with the put function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzule");
+	return (0);
+}
