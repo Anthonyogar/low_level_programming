189dde9adc914d4e5a4f660aa33b3873d65a960a
Anthony Odey <anthonyogar61@gmail.com>
2023-02-17 04:11:16 +0100

    Quote

---
+++ hello_world/quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to the standard error
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fput("i will be good today\" - Travis, 17-2-2023,\n", stdout);
+	return (1);
+}
