d6784a3902e8c0086761e266740cf769fb7cc817
Anthony Odey <anthonyogar61@gmail.com>
2023-02-17 03:29:56 +0100

    printf

---
+++ hello_world/printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using printf function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("until a man understands eternity he will not rise,\n");
+	return (0);
+}
+
