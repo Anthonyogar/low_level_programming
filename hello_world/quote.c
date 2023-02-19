adfd4a1aa33aa6b3f5dadf9360167a0efecb13ca
Anthony Odey <anthonyogar61@gmail.com>
2023-02-19 15:43:54 +0100

    quoted

--- hello_world/quote.c
+++ hello_world/quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	fputs("i will be good today\" - Travis, 17-2-2023,\n", stdout);
-	return (1);
+write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
-
