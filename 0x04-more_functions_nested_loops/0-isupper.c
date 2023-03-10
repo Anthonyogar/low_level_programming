7a72f2b208bf136deedc1eca912748467732a60b
Anthony Odey <anthonyogar61@gmail.com>
2023-03-10 18:03:44 +0100

    Updated files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _isupper -> this is a function to check uppercase character
+* @c: character to check
+* Return: something but success
+*/
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* more_numbers -> printing more numbers
+*/
+void more_numbers(void)
+{
+	int y, z;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (z = 0; z < 15; z++)
+		{
+			if (z >= 10)
+				_putchar((z / 10) + '0');
+			_putchar((z % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
